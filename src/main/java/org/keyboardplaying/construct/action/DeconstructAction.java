fbbde4c65fc8713d8a9623afe5e81d36cbb25254
Cyrille Chopelet <cyrille.chopelet@mines-nancy.org>
2015-07-20 22:19:53 +0200

    Base implementation for XLSX deconstruction

--- src/main/java/org/keyboardplaying/construct/action/DeconstructAction.java
+++ src/main/java/org/keyboardplaying/construct/action/DeconstructAction.java
@@ -1,5 +1,13 @@
 package org.keyboardplaying.construct.action;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 import org.keyboardplaying.construct.ConstructPaths;
 
 /**
@@ -34,8 +42,62 @@ public class DeconstructAction implements Action {
      * @see org.keyboardplaying.construct.action.Action#perform()
      */
     @Override
-    public boolean perform() {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean perform() throws FileNotFoundException, IOException {
+        File deconstructed = new File(ConstructPaths.DECONSTRUCT_PATH);
+        File constructed = new File(ConstructPaths.CONSTRUCT_PATH);
+
+        // Remove previous version and create a new directory
+        if (deconstructed.exists()) {
+            delete(deconstructed);
+        }
+
+        // Unzip XLSX
+        unzip(constructed, deconstructed);
+
+        return true;
+    }
+
+    public void delete(File f) {
+
+        if (f.isDirectory()) {
+            for (File file : f.listFiles()) {
+                delete(file);
+            }
+        } else {
+            f.delete();
+        }
+    }
+
+    public void unzip(File zip, File destination) throws FileNotFoundException, IOException {
+
+        if (!destination.exists()) {
+            destination.mkdir();
+        }
+
+        byte[] buffer = new byte[1024];
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zip))) {
+            // get the zipped file list entry
+            ZipEntry ze = zis.getNextEntry();
+
+            while (ze != null) {
+
+                File entry = new File(destination, ze.getName());
+
+                // create all non exists folders
+                // else you will hit FileNotFoundException for compressed folder
+                new File(entry.getParent()).mkdirs();
+
+                try (FileOutputStream fos = new FileOutputStream(entry)) {
+
+                    int length;
+                    while ((length = zis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, length);
+                    }
+
+                }
+                zis.closeEntry();
+                ze = zis.getNextEntry();
+            }
+        }
     }
 }
